bb94a506e97474d7d7e952c9dcc39362c1fd7049
j.sierra.rodriguez <j.sierra.rodriguez@accenture.com>
2019-09-30 22:12:19 -0500

    - fixed typo

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
 app.use(bodyParser.json());
 
-app.post('/webhook', async (req, res) => {)
+app.post('/webhook', async (req, res) => {
     if(!req.body){
         return res.sendStatus(500);
     }
