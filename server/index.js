64ee0f5f708d2476246227764a059b9a33aef04e
j.sierra.rodriguez <j.sierra.rodriguez@accenture.com>
2019-10-08 13:08:11 -0500

    - added endpoint for alexa skills

--- server/index.js
+++ server/index.js
@@ -46,67 +46,11 @@ app.post('/webhook', async (req, res) => {
 
     return res.json(responseObj);
 });
-
+app.post('/alexaEndpoint', async (req, res) => {
+    console.log(req.body);
+    res.sendStatus(200);
+});
 var port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`Test weahter app listening on port ${port}`);
 });
-
-
-/*
-{
-  "fulfillmentText": "This is a text response",
-  "fulfillmentMessages": [
-    {
-      "card": {
-        "title": "card title",
-        "subtitle": "card text",
-        "imageUri": "https://assistant.google.com/static/images/molecule/Molecule-Formation-stop.png",
-        "buttons": [
-          {
-            "text": "button text",
-            "postback": "https://assistant.google.com/"
-          }
-        ]
-      }
-    }
-  ],
-  "source": "example.com",
-  "payload": {
-    "google": {
-      "expectUserResponse": true,
-      "richResponse": {
-        "items": [
-          {
-            "simpleResponse": {
-              "textToSpeech": "this is a simple response"
-            }
-          }
-        ]
-      }
-    },
-    "facebook": {
-      "text": "Hello, Facebook!"
-    },
-    "slack": {
-      "text": "This is a text response for Slack."
-    }
-  },
-  "outputContexts": [
-    {
-      "name": "projects/${PROJECT_ID}/agent/sessions/${SESSION_ID}/contexts/context name",
-      "lifespanCount": 5,
-      "parameters": {
-        "param": "param value"
-      }
-    }
-  ],
-  "followupEventInput": {
-    "name": "event name",
-    "languageCode": "en-US",
-    "parameters": {
-      "param": "param value"
-    }
-  }
-}
-*/
